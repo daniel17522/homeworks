eafa7772a3d736d7a11cc690a0368a54d7a32e20
daniel17522 <daniel312123312@gmail.com>
2024-08-04 15:55:47 +0300

    hero.py

---
+++ hero.py
@@ -0,0 +1,18 @@
+class SuperHero:
+    people='people'
+    def __init__(self, name, nickname, superpower, health_points, catchphrase):
+        self.name = name
+        self.hp = health_points*2
+        self.nickname = nickname
+        self.superpower = superpower
+        self.catchphrase = catchphrase
+    def info(self):
+        print(f' name: {self.name} catchphrase: {self.catchphrase}')
+    def __str__(self):
+        return f' nickname: {self.nickname} superpower: {self.superpower} health points: {self.hp}'
+    def __len__(self):
+        return len(self.catchphrase)
+Deadpool = SuperHero(' Wade\n','Deadpool\n', 'regenerating\n', 150, "Well, I May Be Super, But Im not hero" )
+Deadpool.info()
+print(Deadpool)
+print(' длинна коронной фразы:', len(Deadpool), 'символов')
